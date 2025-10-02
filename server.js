7ea5165ad88214cfe35509596206d09eebc3d9d5
dianannperla <waafirmelyza@hotmail.com>
2025-10-02 16:54:18 +0100

    Added a simple error handling middleware.

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
